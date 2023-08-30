03ebf444118fd17fcbef6dfc6a76b1070d4336fd
Juniour7 <giftmomanyi97@gmail.com>
2023-08-30 10:07:33 +0300

    Schema completed

---
+++ Server/database/restraunt/index.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const RestrauntSchema = new mongoose.Schema({
+    name: {type: String, required: true},
+    city: {type: String, required: true},
+    address: {type: String, required: true},
+    mapLocation: {type: String, requied: true},
+    cuisine: [String],
+    restrauntTimings: String,
+    contactNumber: Number,
+    website: String,
+    popularDishes: [String],
+    averageost: Number,
+    amenities: [String],
+    menuImages: {
+        type: mongoose.Types.ObjectId,
+        ref: "Images"
+    },
+    menu: {
+        type: mongoose.Types.ObjectId,
+        ref: "Menus"
+    },
+    reviews: [{
+        type: mongoose.Types.ObjectId,
+        ref: "Reviews"
+    }],
+    photos: {type: mongoose.Types.ObjectId, ref: "Images"}
+});
+
+export const RestrauntModel = mongoose.model("Restraunts", RestrauntSchema);
---
+++ Server/database/reviews/index.js
@@ -0,0 +1,17 @@
+import mongoose, { Types } from "mongoose";
+
+const ReviewSchema = new mongoose.Schema({
+    food: {type: mongoose.Types.ObjectId, ref: "Foods"},
+    restraunt: {type: mongoose.Types.ObjectId, ref: "Restraunts"},
+    user: {type: mongoose.Types.ObjectId, ref: "Users"},
+    rating: {type: Number, required: true},
+    reviewText: {type: String, required: true},
+    photos: [
+        {
+            type: mongoose.Types.ObjectId,
+            ref: "Images"
+        }
+    ]
+});
+
+export const ReviewModel = mongoose.model("Reviews", ReviewSchema);
