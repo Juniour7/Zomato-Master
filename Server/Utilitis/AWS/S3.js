e34ac9b371a4c30b486d7e619c752370cef1cc45
Juniour7 <giftmomanyi97@gmail.com>
2023-10-05 20:01:22 +0300

    Pheuks

--- Server/Utilitis/AWS/S3.js
+++ Server/Utilitis/AWS/S3.js
@@ -1,8 +1,8 @@
 import AWS from "aws-sdk";
 
 const s3Bucket = new AWS.S3({
-  accessKeyId: "AKIAULIWZAIBYESDKAHC",
-  secretAccessKey: "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",
+  accessKeyId: "",
+  secretAccessKey: "",
   region: "ap-south-1"
 });
 
